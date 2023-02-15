54f07ea1d0a3b62cf58ca4bc1d1f59764dc4be8c
David Rios <david.r.v@hotmail.com>
2023-02-14 23:36:25 -0300

    Intento de home

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return <h1>Home page</h1>;
+    return (
+
+        <div>
+
+    <h1>Home page en construcción</h1>;
+    <Link>
+        <img className="gif" src="https://64.media.tumblr.com/f0da7c50d726e64c6c0d8a985240b6f4/dfbbb68a95adf2d4-ce/s250x400/afe73dd5883647200c53408018ed3be99b567b80.gifv"/>
+    </Link>
+    </div>
+        )
+    ;
 };
 
 export default Home;
