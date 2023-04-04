8e5fd785d0fe3228132d727cd3c6ccc3c0e75e48
David Rios <david.r.v@hotmail.com>
2023-04-04 01:37:30 -0400

    intento de correcion 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // ...
 import { BrowserRouter } from 'react-router-dom';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
